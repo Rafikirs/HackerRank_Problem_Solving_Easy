897e9e744008a0a87f8d4f4cf945a4c6c67d80d3
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-12 17:17:43 +0200

    Create grading_students.py

---
+++ grading_students.py
@@ -0,0 +1,12 @@
+def gradingStudents(grades):
+    result = []
+    for grade in grades:
+        if grade < 38:
+            result.append(grade)
+        else:
+            next_multiple_of_5 = ((grade // 5) + 1) * 5
+            if next_multiple_of_5 - grade < 3:
+                result.append(next_multiple_of_5)
+            else:
+                result.append(grade)
+    return result
