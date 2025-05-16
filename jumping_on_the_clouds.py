d8513abe2c84211d4a5c298f628b5419bfaf1e4b
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 14:47:52 +0200

    Create jumping_on_the_clouds.py

---
+++ jumping_on_the_clouds.py
@@ -0,0 +1,6 @@
+# Exercise: Jumping on the Clouds
+# URL: https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=true
+# Description:
+# Find the minimum jumps needed to move from the first to the last cloud, 
+# jumping 1 or 2 steps only on safe clouds, avoiding thunderheads
+
