fb5ee1df821dd1bf417a0c3c7e31b9d6ca77ba0d
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:10:41 +0200

    Update subarray_division.py

--- subarray_division.py
+++ subarray_division.py
@@ -1,3 +1,10 @@
+# Exercise: Subarray Division
+# URL: https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true
+# Description: 
+# Lily wants to give Ron a contiguous segment of a chocolate bar where the number of squares equals 
+# his birth month m and the sum of values equals his birth day d. 
+# The task is to count how many such segments exist
+
 def birthday(s, d, m):
     count = 0
     for i in range(len(s) - m + 1):
