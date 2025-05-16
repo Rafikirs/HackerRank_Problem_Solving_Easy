d2ff9dfe29571ad04a6d95b21308099e3594c30e
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:36:24 +0200

    Update compare_the_triplets.py

--- compare_the_triplets.py
+++ compare_the_triplets.py
@@ -1,3 +1,9 @@
+# Exercise: Compare the triplets
+# URL: https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
+# Description: Given two lists of three scores for Alice and Bob, compare each category: 
+# 1 point goes to the person with the higher score; no points for ties. 
+# Return their total points as [Alice_score, Bob_score].
+
 def compareTriplets(a, b):
     alice_score = 0
     bob_score = 0
