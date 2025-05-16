7c11441d8b9f6f32987650be1b095b76036e1fe7
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:31:37 +0200

    Update electronics_shop.py

--- electronics_shop.py
+++ electronics_shop.py
@@ -1,3 +1,10 @@
+# Exercise: Electronics Shop
+# URL: https://www.hackerrank.com/challenges/electronics-shop/problem?isFullScreen=true
+# Description:
+# A person wants to determine the most expensive computer keyboard and USB drive 
+# that can be purchased with a give budget. Given price lists for keyboards and USB drives 
+# and a budget, find the cost to buy them. If it is not possible to buy both items, return -1
+
 def getMoneySpent(keyboards, drives, b):
     max_spent = -1
     for k in keyboards:
