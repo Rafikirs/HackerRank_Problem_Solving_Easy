854c02d5583a669cc22d849853b1606f989a85a3
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:32:52 +0200

    Update simple_array_sum.py

--- simple_array_sum.py
+++ simple_array_sum.py
@@ -1,2 +1,6 @@
+# Exercise: Simple Array Sum
+# URL: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+# Description: Print a simple array sum
+
 def simpleArraySum(ar):
     return sum(ar)
