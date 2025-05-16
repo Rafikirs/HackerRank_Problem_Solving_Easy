928af33520bd0d4b5c42b4164dc0a074188364fb
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:44:37 +0200

    Update mini-max_sum.py

--- mini-max_sum.py
+++ mini-max_sum.py
@@ -1,3 +1,8 @@
+# Exercise: Mini-Max Sum
+# URL: https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
+# Description: Given five positive integers, find the minimum and maximum values 
+# that can be calculated by summing exactly four of the five integers
+
 def miniMaxSum(arr):
     total = sum(arr)
     min_val = total - max(arr)
