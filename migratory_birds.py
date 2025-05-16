e9e92a965b7c20f5dd0946383e27a51cc1eced35
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:16:46 +0200

    Update migratory_birds.py

--- migratory_birds.py
+++ migratory_birds.py
@@ -1,3 +1,10 @@
+# Exercise: Migratory Birds
+# URL: https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScreen=true
+# Description: 
+# Given an array of bird sightings where every element represents a bird type id, 
+# determine the id of the most frequently sighted type. If more than 1 type has been spotted 
+# that maximum amount, return the smallest of their ids.
+
 def migratoryBirds(arr):
     bird_count = {}
     
