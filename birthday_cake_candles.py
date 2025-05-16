dbcc75789d465e3ff41a58def98d6282fafbfb8e
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:48:55 +0200

    Update birthday_cake_candles.py

--- birthday_cake_candles.py
+++ birthday_cake_candles.py
@@ -1,3 +1,7 @@
+# Exercise: Birthday Cake Candles
+# URL: https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+# Description: Find how many times the bigger element of a an array is repeated
+
 def birthdayCakeCandles(candles):
     tallest = max(candles)         
     return candles.count(tallest)  
