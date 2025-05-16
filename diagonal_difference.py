fbb53826eb8f48dfd7b94dfb08b37089db89cf3e
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:40:12 +0200

    Update diagonal_difference.py

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -1,3 +1,7 @@
+# Exercise: Diagonal Difference
+# URL: https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
+# Description: Find the absolute difference of the diagonals of a matrix
+
 def diagonalDifference(arr):
     n = len(arr)
     first_diag = sum(arr[i][i] for i in range(n))
