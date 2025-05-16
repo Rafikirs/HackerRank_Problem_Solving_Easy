8ccb29f223623f7befedab6d2bf903278d3fc723
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 13:58:46 +0200

    Update designer_pdf_viewer.py

--- designer_pdf_viewer.py
+++ designer_pdf_viewer.py
@@ -1,3 +1,9 @@
+# Exercise: Designer PDF Viewer
+# URL: https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
+# Description:
+# Given the heights of each letter, compute the area of the highlighted rectangle 
+# when selecting a word in a PDF viewer. The area is the height of the tallest letter times the word's length.
+
 def designerPdfViewer(h, word):
     max_height = max(h[ord(char) - ord('a')] for char in word)
     return max_height * len(word)
