8dbec03d93d61bd2cd8df72e09f9d2ca7715cb1c
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 14:00:17 +0200

    Update utopian_tree.py

--- utopian_tree.py
+++ utopian_tree.py
@@ -1,3 +1,9 @@
+# Exercise: Utopian Tree
+# URL: https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
+# Description: 
+# Simulate the growth of a Utopian Tree that doubles in height each spring and increases 
+# by 1 meter each summer, starting at 1 meter. Return the tree's height after n growth cycles
+
 def utopianTree(n):
     height = 1
     for cycle in range(n):
