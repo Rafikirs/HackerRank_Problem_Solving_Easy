28f55ef58757b670bcbf35e005b2dbac52f0be13
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:51:02 +0200

    Update time_conversion.py

--- time_conversion.py
+++ time_conversion.py
@@ -1,7 +1,11 @@
+# Exercise: Time Conversion
+# URL: https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+# Description: Given a time in 12-hour AM/PM format, convert it to military (24-hour) time
+
 def timeConversion(s):
-    period = s[-2:]       # AM ou PM
-    hour = int(s[:2])     # Heure en entier
-    rest = s[2:-2]        # Minutes et secondes : ":MM:SS"
+    period = s[-2:]      
+    hour = int(s[:2])     
+    rest = s[2:-2]       
 
     if period == 'AM':
         if hour == 12:
