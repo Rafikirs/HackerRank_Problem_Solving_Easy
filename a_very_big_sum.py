3ec424a4db1db16bf99d34813fa2e606a9721fe1
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:38:32 +0200

    Update a_very_big_sum.py

--- a_very_big_sum.py
+++ a_very_big_sum.py
@@ -1,2 +1,6 @@
+# Exercise: A Very Big Sum
+# URL: https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+# Description: Find the very big sum of elements of an array
+
 def aVeryBigSum(ar):
     return sum(ar)
