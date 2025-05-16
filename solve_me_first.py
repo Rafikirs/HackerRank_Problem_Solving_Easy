b61379478b1cb86f34e7806c416a1a742035da96
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:31:54 +0200

    Update solve_me_first.py

--- solve_me_first.py
+++ solve_me_first.py
@@ -1,2 +1,6 @@
+# Exercise: Solve Me First
+# URL: https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+# Description: Print the sum of two numbers
+
 def solveMeFirst(a, b):
   return a + b
