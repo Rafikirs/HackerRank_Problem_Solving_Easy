cdd803a133c42d1eee55d1b2a5bc6454784869c7
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:00:31 +0200

    Update grading_students.py

--- grading_students.py
+++ grading_students.py
@@ -1,3 +1,9 @@
+# Exercise: Grading Students
+# URL: https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+# Description: Given a list of student grades, round each up to the next multiple of 5 
+# only if the grade is ≥38 and the difference to the next multiple is less than 3.
+# Return the list of final grades after applying this rule
+
 def gradingStudents(grades):
     result = []
     for grade in grades:
