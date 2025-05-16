36b64bc04757962c63a009925d7320e089f24861
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:21:38 +0200

    Update sales_by_match.py

--- sales_by_match.py
+++ sales_by_match.py
@@ -1,3 +1,9 @@
+# Exercise: Sales by Match
+# URL: https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true
+# Description:
+# There is a large pile of socks that must be paired by color. Given an array of integers 
+# representing the color of each sock, determine how many pairs of socks with matching colors there are
+
 def sockMerchant(n, ar):
     sock_count = {}
     pairs = 0
