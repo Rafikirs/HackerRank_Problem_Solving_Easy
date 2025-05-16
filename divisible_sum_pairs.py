f510397dca26ffe90a2380984f2cdcd5bc51321c
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:13:38 +0200

    Update divisible_sum_pairs.py

--- divisible_sum_pairs.py
+++ divisible_sum_pairs.py
@@ -1,3 +1,7 @@
+# Exercise: Divisible Sum Pairs
+# URL: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true
+# Description: Count the number of pairs (i, j) with i < j such that the sum of ar[i] + ar[j] is divisible by k.
+
 def divisibleSumPairs(n, k, ar):
     count = 0
     for i in range(n):
