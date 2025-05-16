4a77309ef134d07a8bc1d5be1426d40ebf33bb44
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:28:18 +0200

    Update counting_valleys.py

--- counting_valleys.py
+++ counting_valleys.py
@@ -1,3 +1,9 @@
+# Exercise: Counting Valleys
+# URL: https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true
+# Description:
+# A valley is a sequence below sea level that starts with a descent and ends when returning to sea level. 
+# Count how many valleys are crossed during a hike
+
 def countingValleys(steps, path):
     altitude = 0
     valleys = 0
