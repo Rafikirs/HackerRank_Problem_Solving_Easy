ed126cd6361fa664409a33902a01f14241d806dc
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:42:45 +0200

    Update staircase.py

--- staircase.py
+++ staircase.py
@@ -1,3 +1,7 @@
+# Exercise: Staircase
+# URL: https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+# Description: Create a staircase of size of a given integer n
+
 def staircase(n):
     for i in range(1, n + 1):
         print(' ' * (n - i) + '#' * i)
