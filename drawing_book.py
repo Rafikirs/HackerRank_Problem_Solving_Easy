abb12f635b5742dfd7d0f0e924f754fa42afae8e
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 12:24:17 +0200

    Update drawing_book.py

--- drawing_book.py
+++ drawing_book.py
@@ -1,3 +1,9 @@
+# Exercise: Drawing Book
+# URL: https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true
+# Description: 
+# Calculate the minimum number of page turns required to reach page p 
+# from either the front or the back of a book with n pages.
+
 def pageCount(n, p):
     from_front = p // 2
     from_back = (n // 2) - (p // 2)
