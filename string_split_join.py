d50b8367deac96f0011e54632df88475c1adcfcf
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-09-29 17:57:20 +0200

    Create string_split_join.py

---
+++ string_split_join.py
@@ -0,0 +1,6 @@
+# Exercise: String Split and Join
+# URL: https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+# Description: Use .split() and .join() on a string
+
+def split_and_join(line):
+    return "-".join(line.split())
