71e2354cb9fdc55e6b9d4b4291d22e1b72dc3fdd
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-14 14:11:54 +0200

    Update sequence_equation.py

--- sequence_equation.py
+++ sequence_equation.py
@@ -1,3 +1,12 @@
 # Exercise: Sequence Equation
 # URL: https://www.hackerrank.com/challenges/permutation-equation/problem?isFullScreen=true
 # Description: Make a permutation of a list based on the positions of its elements
+
+def permutationEquation(p):
+    n = len(p)
+    permuted_p = []
+    
+    for i in range(n):
+        permuted_p.append(p.index(p.index(i+1)+1)+1)
+        
+    return permuted_p
