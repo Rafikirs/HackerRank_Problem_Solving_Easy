158623b497438d90f77c43a0a69122f4b5c48b44
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-14 13:30:45 +0200

    Create between_two_sets.py

---
+++ between_two_sets.py
@@ -0,0 +1,3 @@
+# Exercise: Between Two Sets
+# URL: https://www.hackerrank.com/challenges/between-two-sets/problem?isFullScreen=true
+# Description: Finding how many numbers divide all numbers of a set and are divisors of all numbers of another set
