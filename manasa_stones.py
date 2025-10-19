e6f34d44c2b78272ee68ea680235dee387dc9b30
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-19 13:49:23 +0200

    Update manasa_stones.py

--- manasa_stones.py
+++ manasa_stones.py
@@ -2,9 +2,8 @@
 # URL: https://www.hackerrank.com/challenges/manasa-and-stones/problem?isFullScreen=true
 # Description: Find all possible arrival points based on 2 possible step lengths
 
-from itertools import product
-
 def stones(n, a, b):
-    permutations = list(product([a, b], repeat=n-1))
-    permutations = set(tuple(sorted(perm)) for perm in permutations)
-    return sorted(set(sum(perm) for perm in permutations))
+    results = set()
+    for i in range(n):
+        results.add(i*a + (n-1-i)*b)
+    return sorted(results)
